766dc03b92d087bb94ac695c370bc33981c51b39
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-10-16 00:14:52 +0600

    simple-firebase-authentication

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAC5RfjRvamX7xRLe7oWem5L8YfsxohcCg",
+  authDomain: "simple-firebase-authenti-9d07e.firebaseapp.com",
+  projectId: "simple-firebase-authenti-9d07e",
+  storageBucket: "simple-firebase-authenti-9d07e.appspot.com",
+  messagingSenderId: "916858772724",
+  appId: "1:916858772724:web:b100985eff8105cbd512a1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
